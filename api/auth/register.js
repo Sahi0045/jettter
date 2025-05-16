2d6a6013b20ef9198286ad2fdb605c07fcf2b9c1
Sahith kumar <E23CSEU1350@BENNETT.EDU.IN>
2025-05-16 23:19:06 +0530

    Delete api directory

--- api/auth/register.js
+++
@@ -1,4 +0,0 @@
-// api/auth/register.js - Specific handler for registration endpoint
-import app from '../../server.js';
-
-export default app; 
--- api/index.js
+++
@@ -1,5 +0,0 @@
-// api/index.js - Vercel serverless entry point
-import app from '../server.js';
-
-// Export the Express API for Vercel
-export default app; 
